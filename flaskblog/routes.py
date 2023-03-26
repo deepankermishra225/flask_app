43c987dc1452e0900a3d64ed8661bfd11199503d
deepanker <deepanker.mishra1@gmail.com>
2023-03-26 19:55:13 +0530

    adding profile pic update feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
+import secrets
+import os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.form import RegistrationForm, LoginForm
+from flaskblog.form import RegistrationForm, LoginForm, UpdateRegistrationForm
 from flaskblog.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -70,7 +73,37 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    image_name = random_hex + f_ext
+    image_path = os.path.join(app.root_path, 'static/profile_photos', image_name)
+
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(image_path)
+    return image_name
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+
+    form = UpdateRegistrationForm()
+
+    if form.validate_on_submit():
+        if form.image_file:
+            current_user.image_file = save_picture(form.image_file.data)
+        if form.username.data != current_user.username:
+            current_user.username = form.username.data
+        if form.email.data != current_user.email:
+            current_user.email = form.email.data
+        db.session.commit()
+        flash('Account Info Updated Succesfully', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename=f'profile_photos/{current_user.image_file}')
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
